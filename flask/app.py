8b0c899c4fe2f73f9185c8840477644723077ab8
root <root@sameervivobook>
2022-11-01 18:38:43 +0530

    intial

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session,jsonify
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
